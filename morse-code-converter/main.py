da10987bcd1bc6112d6b4e1432572e48b29946b5
Brett <brettschaus@gmail.com>
2026-08-22 15:33:35 +0200

    Create Morse Code Converter

---
+++ morse-code-converter/main.py
@@ -0,0 +1,31 @@
+## Morse code converter program
+
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-', ' ': '/'}
+
+raw_string = input('Please input what you would like to be converted: ')
+#raw_string = 'Now my code works?'
+
+morse_string = ''
+
+for letter in raw_string.upper():
+    if letter in MORSE_CODE_DICT:
+        morse_string += MORSE_CODE_DICT[letter] + ' '
+
+print(morse_string)
+
+
